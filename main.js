8fe6dd42cc4f79ab00df5f635e15b685baa8d66c
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-06 23:42:12 -0500

    final submit for color clock

--- main.js
+++ main.js
@@ -9,44 +9,63 @@
 
     // This gets a "handle" to the clock div in our HTML
     var clockDiv = document.getElementById("clock");
+  // var background = document.getElementById("#clock[background]");
 // convert minutes, hours, secs to 2 digits if not already
 if (hours < 10) {
   hours = "0" + hours;
 }
    if (minutes < 10) {
     minutes = "0" + minutes;
-  }
+}
   if (seconds < 10) {
   seconds = "0" + seconds;
- }
- hours = parseInt(hours, 10);
- minutes = parseInt(minutes, 10);
- seconds = parseInt(seconds, 10);
- // Then we set the text inside the clock div
- // to the hours, minutes, and seconds of the current time
-
-clockDiv.innerText = "#" + hours + minutes + seconds;
-
 }
 
+clockDiv.innerHTML = "#" + hours + minutes + seconds
+
+//background.innerHTML = clockDiv.innerHTML
+// "clock.background-color"
+
+// var nIntervId;
+//
+//     function changeColor() {
+//       IntervId = setInterval(flashText, 1000);
+//     }
+//
+//     function flashText() {
+//       var Elem = document.getElementById("clock");
+//       Elem.style.color = clockDiv.innerHTML
+//     }
+//   }
+
+//     function changeColor () {
+//
+//     for(var i = 0; i < colors.length; i++) {
+//         colors[i] = color;
+//     }
+//   }
+// }
+    //add the color to the element you want:
+    // document.body.style.backgroundColor = "rgb("+colors[0] + "," + colors[1] + "," + colors[2] + ")";
+    // console.log("event fired. color get:" + colors[0] + colors[1] + colors[2]);}
 // remember that strings have a length, numbers don't
 // can only turn numbers into hex, not strings!
 //when seconds change set off a function to change the bckground color in css
 
-  // var i = 0;
-  // function change() {
-  //   var doc = document.getElementById("clock");
-  //
-  //   doc.style.backgroundColor = "#000000"
-     // must construct number with leading #, six digits, no colons
-  //   i++;
-  //   parseInt("054345", 10);
-  //   console.log(i);
-  //   }
-//doc.style.backgroundColor = "#000000"
-  // This runs the displayTime function the first time
+//   var i = 0;
+//   function change() {
+//     var doc = document.getElementById("clock");
+//
+//     doc.style.backgroundColor = "#000000"
+//
+//     i++;
+//
+//  var doc = document.getElementById(#clock["background"] ;
+// doc.style.clock.background = ""
+//   This runs the displayTime function the first time
 
-
+}
   setInterval(displayTime, 1000);
   displayTime();
+  // "#clock background" = clockDiv.innerHTML;
   //setInterval(change, 1000);
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # IronYard homework 020617
-1. Created a new CSS stylesheet and linked to index.html for Simple Pricing webpage exercise.
-2. Used "float" to present an unordered list of containers for various offers and markets.
-3. Used the console to inspect elements and determine layout using box method
-4. Styled text and graphics in the footer to model
-5. Used pixel perfect to line up target image with rest of page
+1. Created a clock based on new Date object and linked to index.html for color clock webpage exercise.
+2. Added a zero to each number of seconds, minutes, hours if less than 10 using if statements.
+3.
+4.
 5. Create a repo on github for this project and pushed new code to it
+6. gitHub link is https://github.com/rkwyckoff/colorclock020617
